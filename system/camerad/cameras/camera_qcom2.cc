72f2bfe949ddb265a48a24f889bb67e852eddbb0
Comma Device <device@comma.ai>
2022-08-23 16:00:49 -0700

    extra height maybe
    Former-commit-id: 065b9334767a4708f96b8ca9669e07e214494a6d

--- system/camerad/cameras/camera_qcom2.cc
+++ system/camerad/cameras/camera_qcom2.cc
@@ -50,6 +50,7 @@ const size_t FRAME_STRIDE = 640*10/8; // for 10 bit output*/
 //const size_t FRAME_STRIDE = 0xfc0;  // for 12 bit output
 
 const size_t AR0231_REGISTERS_HEIGHT = 2;
+
 // TODO: this extra height is universal and doesn't apply per camera
 const size_t AR0231_STATS_HEIGHT = 2+8;
 
@@ -76,6 +77,7 @@ CameraInfo cameras_supported[CAMERA_ID_MAX] = {
     .frame_width = FRAME_WIDTH,
     .frame_height = FRAME_HEIGHT,
     .frame_stride = FRAME_STRIDE, // (0xa80*12//8)
+    .extra_height = 16, // this right?
     //.frame_stride = 0xfc0, // (0xa80*12//8)
 
     /*.bayer = true,
